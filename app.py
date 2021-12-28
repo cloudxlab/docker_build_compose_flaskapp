a58691d303a13a9df40bc8b621e74358dc41fec2
manojkmgit <manojk.maurya@gmail.com>
2021-12-28 11:52:45 +0530

    changes done

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask,  escape, request
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 
 def main():
-    return "Welcome to Devops Course from - Ashok Singh !"
+    data = f"Welcome to Devops Course here! current host is {socket.gethostname()}"
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4080)
